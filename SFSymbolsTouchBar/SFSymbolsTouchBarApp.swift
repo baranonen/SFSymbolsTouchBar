d9da3f9c515b2b6dc7300cebe47366c313518c7a
Baran Önen <baranonenn@gmail.com>
2021-03-31 22:12:02 +0300

    Initial Commit

---
+++ SFSymbolsTouchBar/SFSymbolsTouchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsTouchBarApp.swift
+//  SFSymbolsTouchBar
+//
+//  Created by Baran Önen on 31.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsTouchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
